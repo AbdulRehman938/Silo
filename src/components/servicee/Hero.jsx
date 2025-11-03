c8290f1d745e95db6235b3b5bbbb21f15c627095
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2025-11-03 19:03:45 +0500

    route fix3

--- src/components/servicee/Hero.jsx
+++ src/components/servicee/Hero.jsx
@@ -5,17 +5,20 @@ import demoVideo from "../../DemoVideo/Demo-Video.mp4";
 import demoVideo2 from "../../DemoVideo/demo2.mp4";
 import Cards from "./Cards";
 import Interested from "./Interested";
+import '../../styles/scaling-overrides.css';
 
 const Hero = () => {
   return (
     <>
       {/* Desktop / larger screens - hidden on small screens */}
       <div className="hidden sm:block w-full h-full">
-        <div className="w-[100vw] 2xl:max-h-[58em] overflow-hidden flex flex-col justify-between -gap-10 items-center 2xl:mt-16 lg:mt-5 lg:h-[88vh] md:mt-5 md:h-[88vh] xl:h-[90vh] xl:mt-10">
-          <VideoPlayer />
+        <div className="w-[100vw] hero-main 2xl:max-h-[58em] overflow-hidden flex flex-col justify-between -gap-10 items-center 2xl:mt-16 lg:mt-5 lg:h-[88vh] md:mt-5 md:h-[88vh] xl:h-[90vh] xl:mt-10">
+              <div style={{ height: '60vh', width: '100%' }} className="flex items-center justify-center">
+                <VideoPlayer containerClassName="video" />
+              </div>
           <div className="flex flex-col 2xl:-mt-16 justify-start gap-1 items-center text-black leading-tight">
-            <h1 className="2xl:text-[15vw] font-black lg:text-[15vw] 2xl:mb-3 md:text-[16vw] md:mb-0 md:0">What we do</h1>
-            <span className="2xl:text-2xl 2xl:-mt-5 2xl:font-bold text-xl text-center 2xl:w-[48vw] lg:w-[60vw] lg:text-lg md:w-[70vw] md:text-base md:mb-5 ">
+            <h1 className="2xl:text-[15vw] hero-title font-black lg:text-[15vw] 2xl:mb-3 md:text-[16vw] md:mb-0 md:0">What we do</h1>
+            <span className="2xl:text-2xl 2xl:-mt-5 2xl:font-medium text-xl text-center 2xl:w-[48vw] lg:w-[60vw] lg:text-lg md:w-[70vw] md:text-base md:mb-5 hero-desc ">
               {" "}
               We make content that cuts through the noise. Strategy, UGC, design,
               and motion, built to get noticed and remembered
