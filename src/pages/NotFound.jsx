73417310f8fe96cc8edcbfb757c3902d8c44b42d
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2025-11-19 15:31:08 +0500

    done layout AR

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
     <div className="min-h-screen bg-white flex flex-col items-center justify-center px-4 sm:px-6 md:px-8 mt-20">
       <div className="text-center max-w-[768px] mx-auto">
         {/* Main Heading */}
-        <h1 className="text-4xl sm:text-5xl md:text-7xl font-epilogue lg:text-8xl xl:text-9xl font-bold text-black leading-tight mb-8 sm:mb-12 md:mb-16">
+        <h1 className="text-5xl sm:text-5xl md:text-7xl font-epilogue lg:text-8xl xl:text-9xl font-bold text-black leading-tight mb-8 sm:mb-12 md:mb-16">
           Well, this is awkward.
         </h1>
         
